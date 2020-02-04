f06845a81b4de2a420a0a18b711d8260168d2c0a
Martin Skarzynski <marskar@gmail.com>
2020-02-04 17:43:18 -0500

    improve script#0

--- 0_edit_scripts.sh
+++ 0_edit_scripts.sh
@@ -1,4 +1,4 @@
-find ~/faes-biof/syllabus -type f -name '*.sh' -print0 | xargs -0 sed -i.bak '
+find ~/faes-biof/syllabus -type f -name '*.sh' ! -name '0_*.sh' -print0 | xargs -0 sed -i.bak '
     s/<year>/2020/;
     s/<semester>/spring/;
     s/<course_code>/biof309/
