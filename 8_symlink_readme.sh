5479ccc6bd144a0f1f0f26662c7c1c942114fc0e
Martin Skarzynski <marskar@gmail.com>
2020-02-05 01:20:19 -0500

    fix script#8

--- 8_symlink_readme.sh
+++ 8_symlink_readme.sh
@@ -1,6 +1,7 @@
 YEAR=<year>
 SEMESTER=<semester>
 COURSE_CODE=<course_code>
+SYLLABUS_NAME=$YEAR-$SEMESTER-$COURSE_CODE-syllabus
 cd ~/$COURSE_CODE/$SEMESTER$YEAR
 ln -fs $SYLLABUS_NAME.md README.md
 git add README.md
