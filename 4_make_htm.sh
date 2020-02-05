2aa4945db2184b0931e160484534c6970af29205
Martin Skarzynski <marskar@gmail.com>
2020-02-04 22:15:56 -0500

    fix script#4

--- 4_make_htm.sh
+++ 4_make_htm.sh
@@ -4,5 +4,5 @@ COURSE_CODE=<course_code>
 SYLLABUS_NAME=$YEAR-$SEMESTER-$COURSE_CODE-syllabus
 cd ~/$COURSE_CODE/$SEMESTER$YEAR
 sed '
-    s/align=">/style="text-align:/
+    s/align="/style="text-align:/
     ' $SYLLABUS_NAME.md | pandoc -o $SYLLABUS_NAME.html
