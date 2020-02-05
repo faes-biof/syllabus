e41b492e1d1fb6d2f1c4810ff1b151c7ee7a3114
Martin Skarzynski <marskar@gmail.com>
2020-02-05 01:01:05 -0500

    rename scripts 1-2,7 and add pdf files to 6-7

--- 6_push_syllabus.sh
+++ 6_push_syllabus.sh
@@ -3,6 +3,6 @@ SEMESTER=spring
 COURSE_CODE=biof309
 SYLLABUS_NAME=$YEAR-$SEMESTER-$COURSE_CODE-syllabus
 cd ~/$COURSE_CODE/$SEMESTER$YEAR
-git add $SYLLABUS_NAME.md $SYLLABUS_NAME.docx $SYLLABUS_NAME.html
+git add $SYLLABUS_NAME.md $SYLLABUS_NAME.docx $SYLLABUS_NAME.html $SYLLABUS_NAME.pdf
 git commit -m "Add FAES syllabus template in md and docx format"
 git push
--- 7_push_others.sh
+++ 7_push_others.sh
@@ -2,6 +2,6 @@ YEAR=<year>
 SEMESTER=<semester>
 COURSE_CODE=<course_code>
 cd ~/$COURSE_CODE/$SEMESTER$YEAR
-git add faes300.png reference.docx
+git add faes300.png reference.docx header.tex
 git commit -m "Add FAES logo and reference docx"
 git push
